ade6afe664e43aabeef33a0f5710901fc2403b08
Monolith <83715191+leuphanalg@users.noreply.github.com>
2021-06-17 14:46:38 +0200

    Add files via upload

--- mouse.js
+++ mouse.js
@@ -2,7 +2,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(windowWidth, windowHeight);
 }
-
 function draw() {
   circle(mouseX+random(-10, 10), mouseY+random(-10, 10), random(50));
   //console.log('mouseIsPressed', mouseIsPressed);
