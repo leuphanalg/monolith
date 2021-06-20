ccac4a57947c1ce083d806fb727322f2f3ba6bb9
Monolith <83715191+leuphanalg@users.noreply.github.com>
2021-06-20 15:38:12 +0200

    Add files via upload

--- mouse.js
+++ mouse.js
@@ -1,11 +1,24 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(windowWidth, windowHeight);
+//let bg;
+//function preload() {
+// bg = loadImage("monolith2.jpg");
+//}
+function setup(){
+  createCanvas(windowWidth,windowHeight);
 }
-function draw() {
+function draw(){
   circle(mouseX+random(-10, 10), mouseY+random(-10, 10), random(50));
   //console.log('mouseIsPressed', mouseIsPressed);
 }
-function mousePressed() {
-  background(random(255), random(255), random(255))
+function mousePressed(){
+  background(random(255), random(255), random(255), 30);
 }
+//let y = 0;
+//function draw() {
+//  background(bg);
+//  stroke(226, 204, 0);
+//  line(0, y, width, y);
+//  y++;
+//  if (y > height) {
+//    y = 0;
+//  }
+//}
